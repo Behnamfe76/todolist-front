04857448aec190fa176e4b1eba7523df8494455c
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-24 13:38:11 +0330

    feat: add new UI components and utility functions for improved design and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import HomeView from '@/views/HomeView.vue';
-// import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <home-view />
-  <!-- <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
+    <RouterView />
 </template>
