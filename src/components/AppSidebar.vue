af1b3359a53d3e13bcda5d2cac604e86ca74d407
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-25 00:16:52 +0330

    feat: add new components for headings, app content, sidebar, breadcrumbs, user info, and appearance tabs

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import NavFooter from '@/components/NavFooter.vue';
+import NavMain from '@/components/NavMain.vue';
+import NavUser from '@/components/NavUser.vue';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import AppLogo from './AppLogo.vue';
+import { RouterLink } from 'vue-router'
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+        icon: LayoutGrid,
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Front Repo',
+        href: 'https://github.com/Behnamfe76/todolist-front',
+        icon: Folder,
+    },
+    {
+        title: 'Backend Repo',
+        href: 'https://github.com/Behnamfe76/todolist-api',
+        icon: Folder,
+    },
+];
+</script>
+
+<template>
+    <Sidebar collapsible="icon" variant="inset">
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" as-child>
+                        <RouterLink to="dashboard">
+                            <AppLogo />
+                        </RouterLink>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent>
+            <NavMain :items="mainNavItems" />
+        </SidebarContent>
+
+        <SidebarFooter>
+            <NavFooter :items="footerNavItems" />
+            <NavUser />
+        </SidebarFooter>
+    </Sidebar>
+    <slot />
+</template>
---
+++ src/components/UserMenuContent.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import UserInfo from '@/components/UserInfo.vue';
+import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
+import { useAuth } from '@/composables/useAuth';
+import type { User } from '@/types';
+import { LogOut, Settings } from 'lucide-vue-next';
+import { RouterLink } from 'vue-router'
+const { logout } = useAuth();
+interface Props {
+    user: User;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <DropdownMenuLabel class="p-0 font-normal">
+        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <UserInfo :user="user" :show-email="true" />
+        </div>
+    </DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuGroup>
+        <DropdownMenuItem :as-child="true">
+            <RouterLink class="block w-full" to="profile/edit" as="button">
+                <Settings class="mr-2 h-4 w-4" />
+                Settings
+            </RouterLink>
+        </DropdownMenuItem>
+    </DropdownMenuGroup>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem :as-child="true">
+        <span @click="logout" class="block w-full">
+            <LogOut class="mr-2 h-4 w-4" />
+            Log out
+        </span>
+    </DropdownMenuItem>
+</template>
