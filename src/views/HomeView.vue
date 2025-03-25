5cabea2359eee99fcebfe1dcd25d30770986a099
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-25 20:37:45 +0330

    fix: update router link in HomeView to use absolute path for dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@ onBeforeMount(async () => {
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
         <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
-                <RouterLink to="dashboard" v-if="isAuthenticated"
+                <RouterLink to="/dashboard" v-if="isAuthenticated"
                     class="inline-block cursor-pointer rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
                     Dashboard
                 </RouterLink>
