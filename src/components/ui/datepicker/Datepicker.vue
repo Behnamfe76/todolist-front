1956be79a2a4d338145cacc2cfd738b8b4651c4a
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-25 22:01:40 +0330

    feat: add Datepicker component with v-model support and styling

---
+++ src/components/ui/datepicker/Datepicker.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+import { useVModel } from '@vueuse/core';
+import type { HTMLAttributes } from 'vue';
+
+const props = defineProps<{
+    defaultValue?: Date;
+    modelValue?: Date;
+    class?: HTMLAttributes['class'];
+}>();
+
+const emits = defineEmits<{
+    (e: 'update:modelValue', payload: string | number): void;
+}>();
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+    passive: true,
+    defaultValue: props.defaultValue,
+});
+</script>
+
+<template>
+    <VueDatePicker v-model="modelValue" :class="cn(
+        'px-[2px]',
+        props.class,
+    )
+        " />
+</template>
+
+<style>
+.dp__pointer {
+    padding-top: 7px !important;
+    padding-bottom: 7px !important;
+}
+</style>
