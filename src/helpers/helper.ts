04b026cc89f3667d69c8122b602a24536371e66d
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 18:18:51 +0330

    feat: add status and priority class attributes for task styling

---
+++ src/helpers/helper.ts
@@ -0,0 +1,13 @@
+export const statusClassAttributes = {
+    done: 'text-teal-400 bg-teal-400/10',
+    todo: 'text-rose-400 bg-rose-400/10',
+    on_progress: 'text-blue-400 bg-blue-400/10',
+    expired: 'text-orange-400 bg-orange-400/10',
+}
+
+export const priorityClassAttributes = {
+    low: 'text-blue-400 bg-blue-400/10',
+    medium: 'text-teal-400 bg-teal-400/10',
+    high: 'text-rose-400 bg-rose-400/10',
+    urgent: 'text-orange-400 bg-orange-400/10',
+}
