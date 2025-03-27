9c15e1ef5d55954a36bcf036dc953f71e1514980
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-27 12:36:31 +0330

    feat: implement pagination and route-based task list updates for improved navigation

--- src/views/tasks/Index.vue
+++ src/views/tasks/Index.vue
@@ -2,9 +2,12 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import TaskListTable from "@/components/tables/TaskListTable.vue"
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, watch } from 'vue';
 import { useApi } from '@/composables/useApi';
+import Pagination from '@/components/Pagination.vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Tasks',
@@ -16,7 +19,7 @@ const { getRequest, loading, error, data } = useApi();
 
 const initTaskList = async () => {
     try {
-        await getRequest('/tasks', { withAuth: true });
+        await getRequest(`/tasks?page=${route.query.page ?? 1}`, { withAuth: true });
     } catch (err) {
         console.log("failed to relative tasks: " + err)
     }
@@ -26,6 +29,10 @@ onBeforeMount(async () => {
     await initTaskList();
 });
 
+watch(route, async ()=>{
+    await initTaskList();
+})
+
 </script>
 
 <template>
@@ -33,7 +40,10 @@ onBeforeMount(async () => {
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl py-4 px-8">
             <!-- table data -->
-            <task-list-table v-if="!loading" :data="data.data.data" />
+             <div v-if="!loading">
+                 <task-list-table :data="data.data.data" />
+                 <Pagination :links="data.data.links" />
+             </div>
             <div v-else>
                 Loading...
             </div>
