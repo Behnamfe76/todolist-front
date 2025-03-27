b40cbcf288ec29f4f026118215cbe0bde5fa49c4
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-27 21:26:43 +0330

    feat: integrate VueSweetalert2 for enhanced alert functionality

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueSweetalert2)
 app.component('VueDatePicker', VueDatePicker)
 app.mount('#app')
