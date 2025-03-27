44f20989e85c4a0b467ccc60b8298908008cb584
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-27 17:16:52 +0330

    feat: add search functionality with debounce for task filtering

--- src/views/tasks/Index.vue
+++ src/views/tasks/Index.vue
@@ -2,11 +2,14 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import TaskListTable from "@/components/tables/TaskListTable.vue"
-import { onBeforeMount, watch } from 'vue';
+import { onBeforeMount, ref, watch } from 'vue';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { useApi } from '@/composables/useApi';
 import Pagination from '@/components/Pagination.vue';
+import { LoaderCircle } from 'lucide-vue-next';
 import { useRoute } from 'vue-router';
-
+import debounce from 'debounce';
 const route = useRoute();
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -14,7 +17,7 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/tasks',
     },
 ];
-
+const taskSearch = ref();
 const { getRequest, loading, error, data } = useApi();
 
 const initTaskList = async () => {
@@ -29,9 +32,14 @@ onBeforeMount(async () => {
     await initTaskList();
 });
 
-watch(route, async ()=>{
+watch(route, async () => {
     await initTaskList();
 })
+
+watch(taskSearch, debounce((value) => {
+    console.log(value)
+
+}, 1000));
 
 </script>
 
@@ -40,10 +48,27 @@ watch(route, async ()=>{
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl py-4 px-8">
             <!-- table data -->
-             <div v-if="!loading">
-                 <task-list-table :data="data.data.data" />
-                 <Pagination :links="data.data.links" />
-             </div>
+            <div v-if="!loading">
+
+                <!-- search/filter section -->
+                <div class="py-4 text-gray-900 dark:text-gray-100">
+                    <div class="mx-8 mt-4 flex flex-col gap-4">
+                        <Label for="search-input">search</Label>
+                        <Input placeholder="looking for a task's title or description?" id="search-input"
+                            v-model="taskSearch" />
+                        <div class="flex gap-4 items-center">
+                            <span class="flex items-center gap-2" v-if="loading">
+                                Loading ...
+                                <LoaderCircle v-if="loading" class="h-4 w-4 animate-spin" />
+
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <task-list-table :data="data.data.data" />
+                <Pagination :links="data.data.links" />
+            </div>
             <div v-else>
                 Loading...
             </div>
